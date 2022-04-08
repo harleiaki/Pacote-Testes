cc38ab5e853a6ceed79381718010f2bcbb539118
harleiaki <96266332+harleiaki@users.noreply.github.com>
2022-04-08 17:47:09 -0300

    Add files via upload

--- Projeto15_ColecoesMarcosMonteiro/src/ExemploRemoveIf.java
+++ Projeto15_ColecoesMarcosMonteiro/src/ExemploRemoveIf.java
@@ -7,11 +7,11 @@ public class ExemploRemoveIf {
 		
 		Set<Produto> cursos = new HashSet<>();
 		
-		cursos.add(new Produto(" Leite em pó ", " Laticínios ", 5.80));
+		cursos.add(new Produto(" Leite em pó ", " Laticínios ",   5.80));
 		cursos.add(new Produto(" Cerveja em lata " , " Bebidas ", 1.89));
-		cursos.add(new Produto(" Detergente " , " Limpeza ", 2.49));
-		cursos.add(new Produto(" Manteiga " , " Laticínios ", 2.90));
-		cursos.add(new Produto(" Pasta de dente " , " Limpeza ", 1.75));
+		cursos.add(new Produto(" Detergente " , " Limpeza ",      2.49));
+		cursos.add(new Produto(" Manteiga " , " Laticínios ",     2.90));
+		cursos.add(new Produto(" Pasta de dente " , " Limpeza ",  1.75));
 		
 		System.out.println("\nista de produtos Antes da remoção: ");
 		cursos.forEach(p -> System.out.println("Produto: " + p));
